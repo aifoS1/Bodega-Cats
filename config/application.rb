9562070d0cf96dbc26f8e1c345860a6e42c87323
Sofia Petsoulakis <petsofia@gmail.com>
2016-08-11 21:34:16 -0400

    imagemagick is now installed in proper place and converts image sizes

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,6 @@ module BodegaCats
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    Paperclip.options[:command_path] = "/Users/S_Petsoulakis/.rbenv/shims/convert"
+    
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
